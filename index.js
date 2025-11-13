a9a26b6e15a4daa7bb3aa34f855bff795cea4f23
Chris Kempton <chris.kempton@leotechnologies.com>
2025-11-13 05:51:33 +0000

    update to add folders and sub directory

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import { write } from './lib/write.js'
 import { fetchDashboards, fetchDashboard, fetchFolders, fetchAlerts} from './lib/grafana.js'
 
+
 const writeFiles = []
 const dashboards = await fetchDashboards()
 const folders = await fetchFolders()
 const alerts = await fetchAlerts()
 
-writeFiles.push(write('folders.json', folders))
-writeFiles.push(write('dashboards.json', dashboards))
-writeFiles.push(write('alerts.json', alerts))
+writeFiles.push(write('data', 'folders.json', folders))
+writeFiles.push(write('data', 'dashboards.json', dashboards))
+writeFiles.push(write('data', 'alerts.json', alerts))
 
 for (const d of dashboards) {
   const db = await fetchDashboard(d.uid)
-  writeFiles.push(write(`${d.uri.replace('db/', '')}.json`, db))
+  writeFiles.push(write('dashboards', `${d.uri.replace('db/', '')}.json`, db))
 }
 
 await Promise.all(writeFiles)
